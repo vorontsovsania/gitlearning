acb34722ab22209d7d594e89bf8d118fb5668192
Alex <Alex@ALEXANDER>
2017-10-11 22:00:29 +0300

    Description property staging

--- GitLearning.ConsoleClient/Models/Product.cs
+++ GitLearning.ConsoleClient/Models/Product.cs
@@ -3,6 +3,7 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; }        
+        public string Description { get; set; }
     }
 }
