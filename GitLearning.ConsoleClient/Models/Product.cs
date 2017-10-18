036ab35cebad7b55061ff159ea898627adf31823
Александр <vorontsovsania@gmail.com>
2017-10-18 09:29:59 +0300

    Create ModificationDatetime property for Product

--- GitLearning.ConsoleClient/Models/Product.cs
+++ GitLearning.ConsoleClient/Models/Product.cs
@@ -1,10 +1,13 @@
-﻿namespace GitLearning.ConsoleClient.Models
+﻿using System;
+
+namespace GitLearning.ConsoleClient.Models
 {
     public class Product
     {
         public int Id { get; set; }
-        public string Title { get; set; }        
+        public string Title { get; set; }
         public string Description { get; set; }
-        public string CategoryId { get; set; }        
+        public string CategoryId { get; set; }
+        public DateTime LastModificationDatetime { get; set; }
     }
 }
