5eb438038c8d8b95a75c1f988a78fecdc7dd238d
Alexander Vorontsov <vorontsovsania@gmail.com>
2021-02-04 09:36:59 +0300

    Add Money value type sketch

---
+++ GitLearning.ConsoleClient/Models/Money.cs
@@ -0,0 +1,12 @@
+﻿namespace GitLearning.ConsoleClient.Models
+{
+    public class Money
+    {
+        public decimal Value { get; }
+
+        public Money(decimal value)
+        {
+            Value = value;
+        }
+    }
+}
