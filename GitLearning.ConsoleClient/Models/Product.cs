2cf24823c9de038527defe9fd74508d9de8656b5
Alexander Vorontsov <vorontsovsania@gmail.com>
2021-04-27 10:57:59 +0300

    Add Owner property to product model

--- GitLearning.ConsoleClient/Models/Product.cs
+++ GitLearning.ConsoleClient/Models/Product.cs
@@ -7,6 +7,7 @@ namespace GitLearning.ConsoleClient.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public decimal Cost { get; set; }
+        public string Owner { get; set; }
         public string Description { get; set; }
         public string CategoryId { get; set; }
         public DateTime LastModificationDatetime { get; set; }
